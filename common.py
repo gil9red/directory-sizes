b693d9d6c1ccee7d20f5ca3f8f73a86bc9f2832b
gil9red <ilya.petrash@inbox.ru>
2025-08-07 21:01:48 +0500

    Рефакторинг. common.py

--- common.py
+++ common.py
@@ -42,13 +42,13 @@ def get_logger(file_name: str, dir_name: Path = config.DIR / "logs") -> logging.
     return log
 
 
-def get_bytes(text: str, units: str = "BKMGTPE") -> float:
+def get_bytes(text: str, units: str = "BKMGTPE") -> int:
     """Возвращает числовое значение в байтах разбирая строки вида: 1 GB, 50 MB и т.п."""
 
     # Возможно, мы просто получили число как строку, тогда не делаем с ней каких-то действий манипуляций,
     # а только превращаем в число и возвращаем
     try:
-        return float(text)
+        return int(float(text))
     except:
         pass
 
@@ -59,11 +59,11 @@ def get_bytes(text: str, units: str = "BKMGTPE") -> float:
     try:
         # Если ошибок не случится, значит получили байты (предположительно),
         # Это может быть строка: "766.00 B"
-        return float(text)
+        return int(float(text))
     except:
         pass
 
-    # NOTE: For '54,7G' -> num='54,7' and unit='G'
+    # NOTE: For '54.7G' -> num='54.7' and unit='G'
     num = float(text[:-1])
     unit = text[-1:]
 
@@ -76,10 +76,10 @@ def get_bytes(text: str, units: str = "BKMGTPE") -> float:
         unit_pow >= 0
     ), f"Unit pow should > 0, unit_pow={unit_pow} unit={unit}. Text={text}."
 
-    return num * (1024**unit_pow)
+    return int(num * (1024**unit_pow))
 
 
-def pretty_file_size(n_size: float) -> str:
+def pretty_file_size(n_size: int) -> str:
     i = 0
     size = n_size
 
@@ -87,13 +87,13 @@ def pretty_file_size(n_size: float) -> str:
         size /= 1024
         i += 1
 
-    return f"{'{:.2f}'.format(size)} {'BKMGTPE'[i]}{('B' if i > 0 else ' ')}"
+    return f"{size:.2f} {'BKMGTPE'[i]}{('B' if i > 0 else '')}"
 
 
 logger = get_logger("directory_sizes_gui")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for value in ["1 B", "1 KB", "1 MB", "1 GB"]:
         total_bytes = get_bytes(value)
         print(value, total_bytes, pretty_file_size(total_bytes))
